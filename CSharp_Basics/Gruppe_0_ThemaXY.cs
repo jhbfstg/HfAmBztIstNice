1b2768e84dd603e5cc4f2ff6dafbc60a33b42b34
hausertim <hausertim@hotmail.ch>
2025-11-21 19:32:11 +0100

    for schleife

--- CSharp_Basics/Gruppe_0_ThemaXY.cs
+++ CSharp_Basics/Gruppe_0_ThemaXY.cs
@@ -3,3 +3,8 @@ Console.WriteLine("║                                      ║");
 Console.WriteLine("║         HfAmBztIstNice               ║");
 Console.WriteLine("║                                      ║");
 Console.WriteLine("╚══════════════════════════════════════╝");
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"This is line number {i}");
+}
